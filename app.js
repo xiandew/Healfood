2cd1bcb19e3257c62e0fd89d66b2d786b7640e5a
Xiande Wen <visiande1998@gmail.com>
2019-04-07 22:26:39 +1000

    Update package.json Add port constant.

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 let express = require('express');
 let app = express();
 
+const PORT = process.env.PORT || 3000;
+
 // Database setup
 // require('./models/db.js');
 
 // Routes setup
 let routes = require('./routes/routes.js');
-app.use('/',routes);
+app.use('/', routes);
 
 // Start the server
-app.listen(3000,function(req,res){
+app.listen(PORT, function(req,res){
     console.log('Express listening on port 3000');
 });
