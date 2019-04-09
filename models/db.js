6ab929e4471389cf2d1e6f90847850e7be663120
Xiande Wen <visiande1998@gmail.com>
2019-04-09 18:15:28 +1000

    Change the db link

--- models/db.js
+++ models/db.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 
 // mongodb+srv://samtafoya:<password>@cluster0-ch4dy.mongodb.net/test?retryWrites=true
 mongoose.connect(
-    'mongodb+srv://samtafoya:artisans@cluster0-ch4dy.mongodb.net/test?retryWrites=true',
+    // 'mongodb+srv://samtafoya:artisans@cluster0-ch4dy.mongodb.net/test?retryWrites=true',
+    "mongodb+srv://xiandew:a1234567@@info30005-7sowv.gcp.mongodb.net/Project?retryWrites=true",
     {
         useNewUrlParser: true
     },
