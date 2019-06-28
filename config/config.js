29973c78441689d3292cb66e49601e5dcf4deccb
Xiande Wen <visiande1998@gmail.com>
2019-06-28 23:27:03 +1000

    Add configDevelopment

--- config/config.js
+++ config/config.js
@@ -1,23 +1,30 @@
-let config = {
-    mongoDB: {
-        dbURI: process.env.mongoDBURI,
-        options: {
-            useNewUrlParser: true,
-            useCreateIndex: true
+let developmentMode = false;
+let config;
+
+if (developmentMode) {
+    config = require("./configDevelopment");
+} else {
+    config = {
+        mongoDB: {
+            dbURI: process.env.mongoDBURI,
+            options: {
+                useNewUrlParser: true,
+                useCreateIndex: true
+            }
+        },
+        cloudinary: {
+            cloud_name: process.env.cloudinaryCloudName,
+            api_key: process.env.cloudinaryApiKey,
+            api_secret: process.env.cloudinaryApiSecret
+        },
+        sendGrid: {
+            service: 'SendGrid',
+            auth: {
+                user: process.env.sendGridUser,
+                pass: process.env.sendGridPass
+            }
         }
-    },
-    cloudinary: {
-        cloud_name: process.env.cloudinaryCloudName,
-        api_key: process.env.cloudinaryApiKey,
-        api_secret: process.env.cloudinaryApiSecret
-    },
-    sendGrid: {
-        service: 'SendGrid',
-        auth: {
-            user: process.env.sendGridUser,
-            pass: process.env.sendGridPass
-        }
-    }
-};
+    };
+}
 
 module.exports = config;
